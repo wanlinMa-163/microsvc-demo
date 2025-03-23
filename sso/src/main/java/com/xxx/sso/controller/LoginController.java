813adf961a85453ff75390c31d03d3c54a331d96
mawl <mawl@sjxm.com>
2025-03-23 22:02:52 +0800

    sso官网示例

--- sso/src/main/java/com/xxx/sso/controller/LoginController.java
+++ sso/src/main/java/com/xxx/sso/controller/LoginController.java
@@ -1,8 +1,5 @@
 package com.xxx.sso.controller;
 
-import org.keycloak.authorization.client.AuthzClient;
-import org.keycloak.representations.idm.authorization.AuthorizationRequest;
-import org.keycloak.representations.idm.authorization.AuthorizationResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +10,6 @@ public class LoginController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam(value = "username") String username, @RequestParam("password") String password) {
-        AuthzClient authzClient = AuthzClient.create();
-        AuthorizationRequest request = new AuthorizationRequest();
-        AuthorizationResponse response = authzClient.authorization(username, password).authorize(request);
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok("");
     }
 }
